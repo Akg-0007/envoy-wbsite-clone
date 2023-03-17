42e9332d9180c37ddf159bb934419ee07ef0a9c8
Akash Gupta <63384864+Akg-0007@users.noreply.github.com>
2023-03-17 22:48:32 +0530

    Add files via upload

---
+++ src/asset/Sass.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+const Sass = () => {
+  return (
+    <div>
+        <header>
+            <div className="container796">
+                <div className="logopart">
+                    <nav>
+                        <ul className="navigation">
+                            <li><a href=""></a>Home</li>
+                            <li><a href=""></a>Home</li>
+                            <li><a href=""></a>Home</li>
+                            <li><a href=""></a>Home</li>
+                            <li><a href=""></a>Home</li>
+                        </ul>
+                    </nav>
+                </div>
+            </div>
+        </header>
+    </div>
+  )
+}
+
+export default Sass
